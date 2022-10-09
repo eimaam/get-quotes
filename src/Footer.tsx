1acc645b7d47e20748e148f1641593a80cdf7a8c
eimaam <imamddahir@gmail.com>
2022-10-09 04:43:16 +0100

    updated profile link in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,11 @@
 export const Footer = () => {
   return (
     <div className='w-100 fixed bottom-0 left-0 right-0 bg-gray-200 text-center'>
-        <h2 className='tracking-wider'><a target="_blank" href="eimaam.dev" > Eimaam &copy; 2022.</a></h2>
+        <h2 className='tracking-wider'>
+          <a target="_blank" href="https://www.eimaam.dev"> 
+          Eimaam &copy; 2022.
+          </a>
+        </h2>
     </div>
   )
 }
