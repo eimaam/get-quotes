538e3bd2fac2f0574c1a9ec27a3a1dca13bd6908
eimaam <imamddahir@gmail.com>
2023-11-24 12:20:12 +0100

    chore: refactor components

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,19 +1,7 @@
 "use client"
 import Link from "next/link";
-import { useEffect } from "react";
 
 export const Footer = () => {
-
-
-//     useEffect(() => {
-// const getQuote = async () => {
-//     const res = await fetch('http://api-get-quotes.vercel.app/api/v1/getquotes', { method: "GET" })
-//     console.log("quotesssssss`", await res.json())
-
-// }
-// getQuote()
-
-//     },[])
 
   return (
     <div className="text-center fixed bottom-0 left-0 right-0 ">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { PT_Mono } from 'next/font/google'
 import './globals.css'
-import { Suspense } from 'react'
-import Loader from '@/loading'
 
 const pt_mono = PT_Mono({ weight: "400", subsets: ['latin']})
 
@@ -18,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <Suspense fallback={<Loader />}> */}
       <body className={pt_mono.className}>{children}</body>
-      {/* </Suspense> */}
     </html>
   )
 }
